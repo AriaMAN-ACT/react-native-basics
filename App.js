5eb91a6b1d421b9c386e3e3314e7ef9cdd954ce1
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-12 21:10:26 +0430

    create input with styles

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, TextInput, Button} from 'react-native';
 
 const App = () => {
     return (
-        <View style={styles.container}>
+        <View>
+            <View style={{padding: 50, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                <TextInput placeholder="Course Goal"
+                           style={{width: '80%', borderBottomColor: 'black', borderBottomWidth: 1, padding: 10, marginBottom: 10}}/>
+                <Button title="Add"/>
+            </View>
         </View>
     );
 };
